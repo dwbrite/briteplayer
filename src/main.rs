937f48faf2555696b108d20a8f57e0d20ce5f948
Devin Brite <devin@dwbrite.com>
2020-07-14 22:30:18 -0400

    demo ListBox with on-focus closures

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,52 @@
+#[macro_use]
+extern crate glib;
 extern crate gtk;
 extern crate gio;
+extern crate gdk;
 
 // To import all needed traits.
 use gtk::prelude::*;
 use gio::prelude::*;
 
+
+use gtk::ResponseType;
+use std::env::args;
 use std::env;
+use std::convert::TryFrom;
+use std::rc::Rc;
+
 
 fn build_ui(app: &gtk::Application) {
-    let btn = gtk::Button::with_label("o hej");
-    let txt = gtk::Label::new(Some(";)"));
-    let vbox = gtk::Box::new(gtk::Orientation::Vertical, 0);
-    vbox.pack_start(&btn, true, true, 0);
-    vbox.pack_start(&btn, true, true, 0);
-    vbox.pack_start(&txt, true, true, 0);
-    vbox.pack_start(&btn, true, true, 0);
+
+    let list = gtk::ListBox::new();
+
+    for i in 0..25 {
+        let txt = gtk::Label::new(Some(&i.to_string()));
+
+        let box_ = gtk::ListBoxRow::new();
+        box_.add(&txt.clone());
+        box_.show_all();
+
+        let txt_rc = Rc::new(txt);
+        box_.connect_event(move |row, e| {
+            if row.clone().has_focus() && e.get_event_type() == gdk::EventType::FocusChange {
+                txt_rc.set_markup("o <i>hej</i>");
+                println!("cool");
+            }
+            Inhibit(false)
+        });
+
+        list.add(&box_.upcast::<gtk::Widget>());
+    }
+
+    let scrollbox = gtk::ScrolledWindow::new::<gtk::Adjustment, gtk::Adjustment>(None, None);
+    scrollbox.add(&list);
+
 
     let window = gtk::ApplicationWindow::new(app);
     window.set_title("test example");
-    window.set_default_size(200, 100);
-    window.add(&vbox);
+    window.set_default_size(600, 400);
+    window.add(&scrollbox);
     window.show_all();
 }
 
@@ -28,9 +55,6 @@ fn main() {
         Some("org.dwbrite.briteplayer"),
         gio::ApplicationFlags::FLAGS_NONE
     ).expect("Application::new failed");
-
-
-
 
     application.connect_activate(|app| {
         build_ui(&app);
