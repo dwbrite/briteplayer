86bc9fdd8b472f08d8fe05f80555438abfc3f5ec
Devin Brite <devin@dwbrite.com>
2020-08-26 19:25:31 -0400

    begin rewrite from scratch

--- src/main.rs
+++ src/main.rs
@@ -1,90 +1,31 @@
-mod nav;
+mod universe;
 
-use crate::nav::Navigation;
-use gtk::prelude::*;
-use gtk::Orientation::Vertical;
-use relm::{connect, Component, Relm, Update, Widget};
-use relm_derive::{widget, Msg};
+use crate::universe::Universe;
+use crossbeam_channel::*;
+use gio::prelude::ApplicationExtManual;
+use gio::ApplicationExt;
+use gtk::{ContainerExt, GtkWindowExt, WidgetExt};
+use std::env;
 
-#[derive(Msg)]
-pub enum Msg {
-    Quit,
-    SelectScene(usize),
-}
+struct LeftNav {}
 
-pub struct UniverseModel {
-    nav: Component<Navigation>,
-    relm: Relm<Universe>,
-}
+pub trait GuiController {
+    type Model;
+    type View;
 
-#[widget]
-impl Widget for Universe {
-    fn model(relm: &Relm<Self>, _: ()) -> UniverseModel {
-        UniverseModel {
-            nav: relm::init::<Navigation>(()).unwrap(),
-            relm: relm.clone(),
-        }
-    }
-
-    fn update(&mut self, event: Msg) {
-        match event {
-            Msg::Quit => gtk::main_quit(),
-            Msg::SelectScene(idx) => {
-                println!("nani?! {}", idx);
-            }
-        }
-    }
-
-    // Create the widgets.
-    view! {
-        #[name="window"]
-        gtk::Window {
-            title: "briteplayer",
-            property_height_request: 720,
-            property_width_request: 1280,
-            #[name="vbox"]
-            gtk::Box {
-                orientation: Vertical,
-                spacing: 0,
-                #[name="scene_panes"]
-                gtk::Paned {
-                    position: 320,
-                    #[name="nav_window"]
-                    gtk::ScrolledWindow {
-
-                    },
-                    gtk::Entry {},
-                },
-                #[name="placeholder"]
-                gtk::Entry {},
-            },
-            delete_event(_, _) => (Msg::Quit, Inhibit(false)),
-
-        },
-    }
-
-    fn init_view(&mut self) {
-        self.vbox
-            .set_child_packing(&self.scene_panes, true, true, 0, gtk::PackType::Start);
-        self.vbox
-            .set_child_packing(&self.placeholder, false, true, 0, gtk::PackType::End);
-
-        // self.scene_panes.add1(self.model.nav.widget());
-        self.nav_window.add(self.model.nav.widget());
-        let l = self.model.nav.widget();
-        let relm = &self.model.relm;
-
-        connect!(relm, l, connect_row_activated(_, b), {
-            Msg::SelectScene(b.get_index() as usize)
-        });
-    }
-}
-
-struct SceneList {
-    scenes: Vec<String>,
-    current_scene: usize,
+    fn view(&self) -> Self::View;
+    fn model(&self) -> Self::Model;
 }
 
 fn main() {
-    Universe::run(()).unwrap();
+    let application = gtk::Application::new(
+        Some("org.dwbrite.briteplayer"),
+        gio::ApplicationFlags::FLAGS_NONE,
+    )
+    .expect("Application::new failed");
+
+    application.connect_activate(move |app| {
+        universe::Universe::create(&app);
+    });
+    application.run(&env::args().collect::<Vec<_>>());
 }
